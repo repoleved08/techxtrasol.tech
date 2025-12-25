e1ef7ade47faf75e72044bca371c226c1e6475d6
repoleved08 <geekbii08@gmail.com>
2025-12-26 01:26:35 +0300

    speed insights

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,3 +1,7 @@
+<script setup lang="ts">
+import { SpeedInsights } from '@vercel/speed-insights/vue';
+</script>
+
 <template>
   <div>
     <AppHeader />
@@ -7,5 +11,6 @@
     </UMain>
 
     <AppFooter />
+    <SpeedInsights />
   </div>
 </template>
