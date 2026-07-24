9776ee3695218a705d62be431b25cbae6786c641
Norman Bii <geekbii08@gmail.com>
2026-07-24 15:50:54 +0300

    fix: add zod schema to blog collection so frontmatter fields are proper DB columns

--- content.config.ts
+++ content.config.ts
@@ -1,10 +1,16 @@
-import { defineCollection, defineContentConfig } from '@nuxt/content'
+import { defineCollection, defineContentConfig, z } from '@nuxt/content'
 
 export default defineContentConfig({
   collections: {
     blog: defineCollection({
       source: 'blog/**',
       type: 'page',
+      schema: z.object({
+        date: z.string(),
+        category: z.string(),
+        readTime: z.string(),
+        image: z.string(),
+      }),
     }),
   },
 })
