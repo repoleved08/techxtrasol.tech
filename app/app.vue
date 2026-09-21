fd5546715ef935081461233627c03f3d9eb9be63
repoleved08 <geekbii08@gmail.com>
2026-09-21 13:24:31 +0300

    perf: preload font subsets for critical LCP font

--- app/app.vue
+++ app/app.vue
@@ -2,6 +2,11 @@
 const { theme, toggleTheme, isLight } = useTheme()
 
 useHead({
+  link: [
+    { rel: 'preload', href: '/_fonts/178SDQEgKlkYIwN3m_zkIjcMBFousbX0HQCZgo0EBNLzf6Q-6gJALEDS7yNhRMCqenJPe8GZSicXSdryDX23b39FgW8.woff2', as: 'font', type: 'font/woff2', crossorigin: 'anonymous' },
+    { rel: 'preload', href: '/_fonts/1A__tf0ZS0Popmdoy0kDZjC9cmK73U4dP6MIlsd_mVE-zPz52wX6aUUJP2ax_uXZbgJ0eM8_rL4hFCIvGE44Ffk.woff2', as: 'font', type: 'font/woff2', crossorigin: 'anonymous' },
+    { rel: 'preload', href: '/_fonts/3zHJNmqdAFbSygXFh60olvDMc9mZf_RpJHm5hhtqFXI-PBGlnsMtfLn7RnCxO5_jJe61l26CMDVuNLtfG9gtTlE.woff2', as: 'font', type: 'font/woff2', crossorigin: 'anonymous' },
+  ],
   script: [
     {
       innerHTML: `(function () {
